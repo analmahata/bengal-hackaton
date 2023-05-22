04503c32be70cdc8425412b5103d1a15e9a0d0dc
Anal Mahata <developer.anal@gmail.com>
2023-05-23 01:33:24 +0530

    Create register form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,3 +27,26 @@ document.querySelector(".home").onmouseleave = () => {
         ".home .home-parallax-img"
     ).style.transform = `translateX(0px) translateY(0px)`;
 };
+
+
+
+
+
+
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
